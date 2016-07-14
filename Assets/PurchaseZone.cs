2992a690f95bd520c718d2dc253a1f7afdae7856
jkarabinos <jkarabinos@middlebury.edu>
2016-07-14 16:13:05 -0400

    changes to the ui and the purchase file

--- Assets/PurchaseZone.cs
+++ Assets/PurchaseZone.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
 
 //John Karabinos
 
-public class PurchaseZone : MonoBehaviour {
+public class PurchaseZone : MonoBehaviour, IPointerClickHandler  {
+
+
+	public void OnPointerClick(PointerEventData eventData){
+		Debug.Log("clicked a card to buy");
+
+
+		GameObject card = eventData.pointerEnter;
+		var draggable = card.GetComponent<Draggable>();
+		
+		if ( draggable.cost > 3 ){
+			//for testing purposes
+			Debug.Log( "I can buy this card" );
+		}else{
+			Debug.Log( "this card costs 3 or less" ); 
+		}
+
+	}
+
+
 
 	// Use this for initialization
 	void Start () {
